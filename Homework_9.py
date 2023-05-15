21033720063ffebeaf6d56df99d37cc7a3984ae2
pattrycjag <126571601+pattrycjag@users.noreply.github.com>
2023-05-15 22:11:18 +0200

    Update Homework_9.py

--- Homework_9.py
+++ Homework_9.py
@@ -18,3 +18,28 @@ plt.legend()
 plt.show()
 
 print("Ex.9.2")
+n = 100
+points = np.random.rand(n, 2)
+
+colors = []
+marker_areas = []
+
+for point in points:
+    x, y = point
+    distance = np.sqrt(x**2 + y**2)
+    if distance < 1:
+        colors.append('green')
+    else:
+        colors.append('red')
+    marker_area = np.abs(x) + np.abs(y)
+    marker_areas.append(marker_area)
+
+plt.scatter(points[:, 0], points[:, 1], c=colors, s=np.array(marker_areas)*100, alpha=0.5)
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Random Points')
+plt.grid(True)
+
+plt.show()
