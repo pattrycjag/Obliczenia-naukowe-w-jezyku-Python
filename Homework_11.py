cd737d01dd345eaa0d839723cbc34b6bf2acfb90
pattrycjag <126571601+pattrycjag@users.noreply.github.com>
2023-06-05 16:44:13 +0200

    Update Homework_11.py

--- Homework_11.py
+++ Homework_11.py
@@ -1,3 +1,4 @@
+##11.1
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -31,3 +32,37 @@ plt.ylabel("time")
 plt.imshow(u, cmap='hot', interpolation='nearest')
 plt.colorbar()
 plt.show()
+
+##11.2
+Nx, Nt, L, T, c = 20, 100, 1.0, 2.0, 1.0
+t = np.linspace(0, T, num=Nt+1, dtype=float)
+x = np.linspace(0, L, num=Nx+1, dtype=float)
+dx = x[1] - x[0]
+dt = t[1] - t[0]
+r = (c * dt / dx) ** 2
+print("r =", r)
+assert r < 1
+
+u = np.empty((Nx+1, Nt+1), dtype=float)
+
+u[:, 0] = np.abs(x - L/2) - L/4  #fala, która jest symetryczna względem środka odcinka [0, L] i ma wartość maksymalną w środku odcinka.
+
+# boundary condition, x=0 and x=L
+u[0, :] = 0.0
+u[Nx, :] = 0.0
+
+# initial condition j=1
+u[1:-1, 1] = u[1:-1, 0] + (r * 0.5) * (u[:-2, 0] - 2 * u[1:-1, 0] + u[2:, 0])
+
+for j in range(1, Nt):
+    u[1:-1, j+1] = -u[1:-1, j-1] + 2 * u[1:-1, j] + r * (u[:-2, j] - 2 * u[1:-1, j] + u[2:, j])
+
+plt.title("1D wave equation")
+plt.xlabel("t")
+plt.ylabel("x")
+
+plt.imshow(u, cmap='hot', interpolation='nearest')
+
+plt.colorbar()
+plt.show()
+
