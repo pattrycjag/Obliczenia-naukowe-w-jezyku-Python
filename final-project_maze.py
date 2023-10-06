27127b24fc74fa2a343e92b135e6e0df19fba375
pattrycjag <126571601+pattrycjag@users.noreply.github.com>
2023-10-06 11:07:51 +0200

    Update final-project_maze.py

--- final-project_maze.py
+++ final-project_maze.py
@@ -48,6 +48,68 @@ def generate_randomized_dfs_maze(width, height):
 
     return maze
 
+# Function to generate a maze using Randomized Kruskal's algorithm
+def generate_randomized_kruskal_maze(width, height):
+    maze = [[WALL] * (2 * width + 1) for _ in range(2 * height + 1)]
+
+    walls = []
+
+    for y in range(height):
+        for x in range(width):
+            maze[2 * y + 1][2 * x + 1] = PATH
+
+            if y > 0:
+                walls.append(((x, y), (x, y - 1)))
+            if y < height - 1:
+                walls.append(((x, y), (x, y + 1)))
+
+            if x > 0:
+                walls.append(((x, y), (x - 1, y)))
+            if x < width - 1:
+                walls.append(((x, y), (x + 1, y)))
+
+    def find_set(cell):
+        for i, s in enumerate(sets):
+            if cell in s:
+                return i
+        return None
+
+    sets = [{(x, y)} for y in range(height) for x in range(width)]
+
+    while walls:
+        wall_index = random.randrange(len(walls))
+        cell1, cell2 = walls[wall_index]
+        set1_idx = find_set(cell1)
+        set2_idx = find_set(cell2)
+
+        if set1_idx is not None and set2_idx is not None and set1_idx != set2_idx:
+            set1 = sets[set1_idx]
+            set2 = sets[set2_idx]
+
+            set1.update(set2)
+            sets.pop(set2_idx)
+
+            x1, y1 = cell1
+            x2, y2 = cell2
+            maze[y1 + y2 + 1][x1 + x2 + 1] = PATH
+
+        walls.pop(wall_index)
+
+    # Start-point
+    start_x, start_y = random.randint(0, width - 1), random.randint(0, height - 1)
+    start_x = 2 * start_x + 1
+    start_y = 2 * start_y + 1
+    maze[start_y][start_x] = START
+
+    # End-point
+    end_x, end_y = random.randint(0, width - 1), random.randint(0, height - 1)
+    end_x = 2 * end_x + 1
+    end_y = 2 * end_y + 1
+    maze[end_y][end_x] = END
+
+    return maze
+
+# Function to generate a maze image
 def generate_maze_image(maze, file_name, cell_size=20):
     width = len(maze[0])
     height = len(maze)
@@ -76,29 +138,37 @@ def generate_maze_image(maze, file_name, cell_size=20):
 
     image.save(file_name)
 
+# Main menu function
 def main_menu():
     print("Maze Generation Menu:")
     print("1. Generate and Save Randomized Depth-First Search Maze")
-    print("2. Quit")
+    print("2. Generate and Save Randomized Kruskal's Maze")
+    print("3. Quit")
 
     while True:
-        choice = input("Enter your choice (1-2): ")
+        choice = input("Enter your choice (1-3): ")
         if choice.isdigit():
             choice = int(choice)
-            if 1 <= choice <= 2:
+            if 1 <= choice <= 3:
                 return choice
-        print("Invalid choice. Please enter a valid option (1-2).")
+        print("Invalid choice. Please enter a valid option (1-3).")
 
+# Main function
 def main():
     while True:
         choice = main_menu()
 
         if choice == 1:
-            width = int(input("Enter the width of the maze (default is 10): ") or 10)
-            height = int(input("Enter the height of the maze (default is 10): ") or 10)
+            width = int(input("Enter the width of the DFS maze (default is 10): ") or 10)
+            height = int(input("Enter the height of the DFS maze (default is 10): ") or 10)
             maze = generate_randomized_dfs_maze(width, height)
             maze_name = "maze_dfs.png"
         elif choice == 2:
+            width = int(input("Enter the width of the Kruskal's maze (default is 10): ") or 10)
+            height = int(input("Enter the height of the Kruskal's maze (default is 10): ") or 10)
+            maze = generate_randomized_kruskal_maze(width, height)
+            maze_name = "maze_kruskal.png"
+        elif choice == 3:
             break
 
         cell_size = int(input("Enter the cell size for visualization (default is 20): ") or 20)
